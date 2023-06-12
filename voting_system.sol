7052f91083805326737ddd98241728b7aef71204
RohanS1810 <120509507+RohanS1810@users.noreply.github.com>
2023-06-12 17:47:35 -0400

    Update voting_system.sol

--- voting_system.sol
+++ voting_system.sol
@@ -1,6 +1,10 @@
+//This sets the Solidity version pragma and declares the start of the Voting contract.
 pragma solidity >=0.7.0 <0.9.0;
 contract Voting {
-    struct Voter {
+
+//This defines two struct types: Voter and Proposal. Voter represents a voter and contains information about whether they have voted or not.
+Proposal represents a proposal and contains the name of the proposal and the number of votes it has received.
+   struct Voter {
         bool hasVoted;
         uint votedProposalIndex;
     }
@@ -9,14 +13,18 @@ contract Voting {
         string name;
         uint voteCount;
     }
+//This declares three state variables: chairperson, voters, and proposals.
 
     address public chairperson;
     mapping(address => Voter) public voters;
     Proposal[] public proposals;
-
+    
     enum State { Created, Voting, Ended }
     State public state;
 
+//This defines two custom modifiers: onlyChairperson and inState. The onlyChairperson modifier checks whether the caller of a function is 
+the chairperson address. The inState modifier checks whether the current state of the contract matches the state that was said as an argument.
+    
     modifier onlyChairperson() {
         require(msg.sender == chairperson, "Only the chairperson can perform this action");
         _;
@@ -36,18 +44,21 @@ contract Voting {
         }
     }
 
+//This function can be called by the chairperson to start the voting process.
+
     function startVote() public onlyChairperson inState(State.Created) {
         state = State.Voting;
     }
+
+//This function does the opposite and can end the voting process.
 
     function endVote() public onlyChairperson inState(State.Voting) {
         state = State.Ended;
     }
 
-    function giveRightToVote(address voter) public onlyChairperson inState(State.Created) {
-        require(!voters[voter].hasVoted, "The voter has already voted.");
-        voters[voter].hasVoted = false;
-    }
+//This allows a registered voter to cast their vote for a specific proposal indicated by its proposalIndex. 
+It ensures that the voter has not already voted and that the proposalIndex is valid. It basically safe proofs the voting process and it
+won't allow someone to vote twice.
 
     function vote(uint proposalIndex) public inState(State.Voting) {
         Voter storage voter = voters[msg.sender];
@@ -59,6 +70,8 @@ contract Voting {
 
         proposals[proposalIndex].voteCount++;
     }
+
+//This basically concludes the voting process. It determines the proposal with the highest vote count and returns its name.
 
     function winningProposal() public view inState(State.Ended) returns (string memory winnerName_) {
         uint winningVoteCount = 0;
